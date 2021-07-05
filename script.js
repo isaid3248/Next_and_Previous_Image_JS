c64c752c413623ba88c784233be05773c076756a
isaid3248 <isaid3248@gmail.com>
2021-07-05 22:44:48 +0300

    JS

---
+++ script.js
@@ -0,0 +1,24 @@
+var arr = new collection(['google.png', 'facebook.png', 'linkedin.png', 'twitter.png', 'apple.jpg', 'instagram.jpg']);
+
+function collection(imgs) {
+  this.imgs = imgs;
+  this.i = 0;
+  this.next = function(element) {
+    var img = document.getElementById(element)
+    this.i++;
+    if (this.i == imgs.length) {
+      this.i = 0;
+    }
+    img.src = imgs[this.i];
+
+
+  }
+  this.prev = function(element) {
+    var img = document.getElementById(element);
+    this.i--;
+    if (this.i < 0) {
+      this.i = imgs.length - 1;
+    }
+    img.src = imgs[this.i];
+  }
+}
