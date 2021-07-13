a655a6caba0e0632c8a27da37a9b0b14e2bcbb39
isaid3248 <isaid3248@gmail.com>
2021-07-14 00:24:59 +0300

    JS

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var arr = new collection(['google.png', 'facebook.png', 'linkedin.png', 'twitter.png', 'apple.jpg', 'instagram.jpg']);
+var arr = new collection([
+  "google.png",
+  "facebook.png",
+  "linkedin.png",
+  "twitter.png",
+  "apple.jpg",
+  "instagram.jpg",
+]);
 
 function collection(imgs) {
   this.imgs = imgs;
   this.i = 0;
-  this.next = function(element) {
-    var img = document.getElementById(element)
+  this.next = function (element) {
+    var img = document.getElementById(element);
     this.i++;
     if (this.i == imgs.length) {
       this.i = 0;
     }
     img.src = imgs[this.i];
-
-
-  }
-  this.prev = function(element) {
+  };
+  
+  this.prev = function (element) {
     var img = document.getElementById(element);
     this.i--;
     if (this.i < 0) {
       this.i = imgs.length - 1;
     }
     img.src = imgs[this.i];
-  }
+  };
 }
